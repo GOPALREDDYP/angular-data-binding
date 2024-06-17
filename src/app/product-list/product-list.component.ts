9bc3ddb217562f4c040b71c331714e934b1c7ab4
gopal reddy p <gopalreddy.pothula@gmail.com>
2024-06-17 06:22:09 +0530

    ngFor changes

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,36 +7,5 @@ import { Component } from '@angular/core';
 })
 
 export class ProductListComponent {
-// name="John Smith";
-addToCart:number = 0;
-product = {
-  name: 'iPhone X',
-  price: 789,
-  color: 'Black',
-  discount: 8.5,
-  inStock: 10,
-  pImage: '/src/assets/Images/iphone.png' 
-}
-getDiscountedPrice() {
-  return this.product.price - (this.product.price * this.product.discount / 100)
-}
-
-onNameChange(event: any){
-  // this.name = event.target.value;
- // console.log(event.target.value);
-}
-
-decrementCartValue(){
-  if(this.addToCart > 0){
-    this.addToCart--;
-  }
-  
-}
-
-incrementCartValue(){
-  if(this.addToCart < this.product.inStock){
-    this.addToCart++;
-  }
-  
-}
+  nameList:string[] =['Mark','Steve','Merry','John']; 
 }
