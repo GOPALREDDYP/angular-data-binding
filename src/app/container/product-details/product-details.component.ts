8e227accab86fdacacd550b7b14f47ce0cbecc61
gopal reddy p <gopalreddy.pothula@gmail.com>
2024-06-20 00:44:16 +0530

    ViewChild Decorator

--- src/app/container/product-details/product-details.component.ts
+++ src/app/container/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../Models/Product';
+import { ProductListComponent } from '../product-list/product-list.component';
 
 @Component({
   selector: 'product-details',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
+  product:Product;
+  @Input() productListComponent:ProductListComponent;
 
+  ngOnInit()
+  {
+
+    this.product =this.productListComponent.selectedProduct;
+  }
+
+  getColor(name) { 
+    switch (name) {
+      case 'White':
+        return 'green';
+      case 'Blue':
+        return 'blue';
+      case 'Red':
+        return 'Red';
+      case 'Black':
+        return 'black';
+      case 'Brown':
+        return 'yellow';
+      default:
+          return 'green';
+          
+    }
+    // color: ["White", "Blue", "Black", "Brown", "Red"],
+  }
 }
